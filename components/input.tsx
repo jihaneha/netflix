21411ae241b28c8da3a8836b0d597af6ca1b437a
jihaneha <ghamidou800@gmail.com>
2023-12-14 22:11:44 +0100

    fix/ authPage

--- components/input.tsx
+++ components/input.tsx
@@ -27,7 +27,7 @@ const Input:React.FC<InputProps> =({
              focus:ring-0
              peer" 
             placeholder=" "/>
-          <label htmlfor={id} className="absolute text-md text-zinc-400 duration-150 transform-translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3" >
+          <label htmlFor={id} className="absolute text-md text-zinc-400 duration-150 transform-translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3" >
 {label}
           </label>
         </div>
