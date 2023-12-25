8a4778f09f1d217e5331868caa11e62a988dc3f7
jihaneha <ghamidou800@gmail.com>
2023-12-25 19:33:35 +0100

    feat/ getCurrentUser

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 
+import { NextPageContext } from 'next'
+import {getSession, signOut} from 'next-auth/react'
+
+
 
 export default function Home() {
   return (
-    <>Netflix</>
+    <>
+    <h1 className="text-4xl text-green-500">Netflix</h1>
+    <button className='h-10 w-full bg-white' >Logout</button>
+    </>
   )
 }
