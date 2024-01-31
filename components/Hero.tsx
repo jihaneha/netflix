b6d195afe9ff6c41f62cabe6ebd054e286227b35
jihaneha <ghamidou800@gmail.com>
2024-01-31 20:46:42 +0100

    fix/ hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface HeroProps {
+  title: string;
+  content: string;
+  img: string;
+}
+
+const Hero: React.FC<HeroProps> = ({ title, content, img }) => {
+  return (
+    <div className="w-full border-b-8 border-solid border-gray-600 p-20">
+      <div className="w-full h-full   container max-w-5xl mx-auto">
+        <div className="flex flex-wrap justify-center items-center mx-auto pt-10">
+          <div className=" text-center sm:text-left sm:w-1/2 ">
+            <h2 className="lg:text-5xl sm:text-4xl font-black my-6 text-3xl">
+              {title}
+            </h2>
+            <p className="md:text-2xl">{content}</p>
+          </div>
+          <div className="p-10 w-full sm:w-1/2 ">
+            <img src={img} alt={title} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
